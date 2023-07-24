0c2d98139c0c272a774554ebfbc7bcc0a80124b0
Fekete Balazs <balazs.fekete@stud.ubbcluj.ro>
2023-07-24 17:01:30 +0300

    ITL-8: Added the title

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container mx-auto text-center p-8 flex justify-between w-[40rem]">
         
+        <h1 class="text-7xl font-primary font-semibold text-black">
+            To do list
+        </h1>
         
     </div>
 </template>
