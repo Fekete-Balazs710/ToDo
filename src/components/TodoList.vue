3154cea59d75b1cf77c0e5c17cb711d477f3a968
Fekete Balazs <balazs.fekete@stud.ubbcluj.ro>
2023-07-24 10:11:48 +0300

    Rewrote app using Tailwind CSS
    - Deleted <style> tag with CSS codes
    
    - Installed Tailwind CSS 
    
    - Added Tailwind CSS styling to html elements

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,27 +1,29 @@
 <template>
-    <h1>ToDo App</h1>
+  <div class="container mx-auto text-center p-8">
+    <h1 class="text-3xl font-bold">ToDo App</h1>
     <p>Manage your daily activity</p>
-    <form class="inputForm" @submit.prevent="addTodo()">
+    <form class="my-8" @submit.prevent="addTodo()">
         <input type="text"
                placeholder="Add todo element..."
-               class="inputField"
+               class="p-5 shadow border rounded px-3"
                v-model="inputValue"
                required/>
         <br>
-        <button type="submit" class="formButton">Add to list</button>
+        <button type="submit" class="mt-5 bg-blue-500 hover:bg-blue-700 p-3 btn btn-blue text-white font-bold rounded-full">Add to list</button>
     </form>
 
-    <button class="clearButton" @click="clearTodo()">Clear</button>
+    <button class="bg-blue-400 hover:bg-blue-500 py-3 px-5 btn btn-blue text-white font-bold rounded-full" @click="clearTodo()">Clear</button>
 
-    <ul class="todoList">
-        <li v-for="todo in todos" :key="todo" class="todoListElement">
+    <ul class="mt-5">
+        <li v-for="todo in todos" :key="todo" class="p-5 shadow border rounded mt-3 flex justify-between">
             {{ todo }}
             <button
-                class="deleteButton"
+                class="btn bg-red-400 hover:bg-red-600 px-2 text-white font-bold rounded-md"
                 @click="removeTodo(todo)"
             >x</button>
         </li>
     </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -39,7 +41,7 @@ let todos = ref<string[]>([
 let inputValue = ref('');
 
 //Add function to add elements to existing array of todos
-function addTodo(): void {
+function addTodo() {
   if (inputValue.value) {
     todos.value.push(inputValue.value);
     inputValue.value = ''; // Clear the input field
@@ -47,65 +49,19 @@ function addTodo(): void {
 }
 
 //Delete function to remove elements from todos array
-function removeTodo(todo: string): void {
+function removeTodo(todo: string) {
   const index = todos.value.indexOf(todo);
   if (index !== -1) {
     todos.value.splice(index, 1);
-    
+
     // Updating the todos array to trigger reactivity
     todos.value = todos.value.slice();
   }
 }
 
 //Clear function to remove all elements from todos array
-function clearTodo(): void {
-    todos.value.splice(0, todos.value.length);
+function clearTodo() {
+    todos.value = [];
 }   
 
 </script>
-
-<style>
-.todoListElement {
-    list-style: none;
-    padding: 1rem;
-    background-color: rgb(237, 237, 237);
-    margin-top: 0.5rem;
-    border-radius: 1rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 20rem;
-}
-
-.todoList {
-    margin: auto;
-    padding: 2rem;
-}
-
-.inputForm {
-    padding: 2rem;
-}
-
-.inputField {
-    height: 2rem;
-    padding: 1rem;
-    width: 14rem;
-    border-radius: 0.5rem;
-}
-
-.formButton {
-    padding: 1rem;
-    margin-top: 1rem;
-}
-
-.clearButton {
-    padding: 1rem;
-}
-
-.deleteButton {
-    background-color: #DC4C64;
-    color: white;
-    padding: 0.5rem;
-    border-radius: 0.3rem;
-}
-</style>
