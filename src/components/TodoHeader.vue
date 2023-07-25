0845ffc8cd17642d3db6b629089635ebcbbb309b
Fekete Balazs <balazs.fekete@stud.ubbcluj.ro>
2023-07-25 10:34:59 +0300

    Corrected ITL-1 code based on review
    - Wrote two additional components for the svgs in the header
    
    - Wrote one component for the add button

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -5,22 +5,14 @@
             To do list
         </h1>
 
-        <div class="relative flex items-center" @click="todoCreate()">
-            <div class="absolute w-full h-full flex items-center justify-center shrink-0">
-                <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 29 29" fill="none">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M15.0013 5.08246H13.3607C13.2149 5.08246 13.142 5.15538 13.142 5.30121V13.8871H4.99353C4.8477 13.8871 4.77478 13.9601 4.77478 14.1059V15.7465C4.77478 15.8924 4.8477 15.9653 4.99353 15.9653H13.142V24.5512C13.142 24.697 13.2149 24.77 13.3607 24.77H15.0013C15.1472 24.77 15.2201 24.697 15.2201 24.5512V15.9653H23.3685C23.5144 15.9653 23.5873 15.8924 23.5873 15.7465V14.1059C23.5873 13.9601 23.5144 13.8871 23.3685 13.8871H15.2201V5.30121C15.2201 5.15538 15.1472 5.08246 15.0013 5.08246Z" fill="white"/>
-                </svg>
-            </div>
-            
-            <svg xmlns="http://www.w3.org/2000/svg" width="61" height="60" viewBox="0 0 61 60" fill="none">
-                <circle cx="30.1828" cy="29.926" r="29.8221" fill="#38CB89"/>
-            </svg>
-        </div>
+        <AddTodoButton @addTodo="todoCreate"/>
 
     </div>
 </template>
 
 <script setup lang="ts">
+
+import AddTodoButton from './AddTodoButton.vue'
 
 //Add emit to modify data in parent component
 const emit = defineEmits(['addTodo'])
