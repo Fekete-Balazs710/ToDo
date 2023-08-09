ff562e3fd678669c8864526a3e0e7c6fc9da746b
Fekete Balazs <balazs.fekete@stud.ubbcluj.ro>
2023-08-09 09:37:29 +0300

    Corrected ITL-8 code based on review
    - Added .env file with MONGO_CONNECTION_STRING and PORT variables
    - Read the .env variables in server.js file

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ import express from 'express';
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import TodosRoute from './router/todos.js';
+import dotenv from 'dotenv'; 
 
 //create the express app
 const app = express();
+
+//Load the environment variables from .env file
+dotenv.config();
 
 //Handle CORS + middleware
 app.use(function(req, res, next) {
@@ -15,7 +19,7 @@ app.use(function(req, res, next) {
 })
 
 //database connection
-const uri = "mongodb+srv://feketebalazs710:nimbusz2000@cluster0.xrxiki8.mongodb.net/";
+const uri = process.env.MONGO_CONNECTION_STRING;
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -32,9 +36,10 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.use('/todos', TodosRoute)
+app.use('/todoapp/todos', TodosRoute)
 
-//start the server at port 3000
-app.listen(3000, () => {
-    console.log("Listening at port 3000")
+//start the server at the given port from .env file
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log("Listening at port " + port)
 });
